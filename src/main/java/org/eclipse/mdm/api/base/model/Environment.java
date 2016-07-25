4e3ed0ef998d741d1e6e689c3adb5be592c42e2b
Viktor Stöhr <viktor.stoehr@gigatronik.com>
2016-07-25 10:04:00 +0200

    missing getter/setter in Environment added

--- src/main/java/org/eclipse/mdm/api/base/model/Environment.java
+++ src/main/java/org/eclipse/mdm/api/base/model/Environment.java
@@ -16,7 +16,41 @@ package org.eclipse.mdm.api.base.model;
  * @author Viktor Stoehr, Gigatronik Ingolstadt GmbH
  * @author Sebastian Dirsch, Gigatronik Ingolstadt GmbH
  */
-public final class Environment extends BaseEntity {
+public final class Environment extends BaseEntity implements Datable, Describable {
+
+	// ======================================================================
+	// Class variables
+	// ======================================================================
+
+	/**
+	 * The 'MaxTestLevel' attribute name.
+	 */
+	public static final String ATTR_MAX_TEST_LEVEL = "Max_test_level";
+
+	/**
+	 * The 'BaseModelVersion' attribute name.
+	 */
+	public static final String ATTR_BASE_MODEL_VERSION = "Base_model_version";
+
+	/**
+	 * The 'AppModelVersion' attribute name.
+	 */
+	public static final String ATTR_APP_MODEL_VERSION = "AppModelVersion";
+
+	/**
+	 * The 'AppModelType' attribute name.
+	 */
+	public static final String ATTR_APP_MODEL_TYPE = "AppModelType";
+
+	/**
+	 * The 'Timezone' attribute name.
+	 */
+	public static final String ATTR_TIMEZONE = "Timezone";
+
+	/**
+	 * The 'MeaningOfAliases' attribute name.
+	 */
+	public static final String ATTR_MEANING_OF_ALIASES = "MeaningOfAliases";
 
 	// ======================================================================
 	// Constructors
@@ -29,6 +63,34 @@ public final class Environment extends BaseEntity {
 	 */
 	Environment(Core core) {
 		super(core);
+	}
+
+	// ======================================================================
+	// Public methods
+	// ======================================================================
+
+	public Integer getMaxTestLevel() {
+		return getValue(ATTR_MAX_TEST_LEVEL).extract();
+	}
+
+	public String getBaseModelVersion() {
+		return getValue(ATTR_BASE_MODEL_VERSION).extract();
+	}
+
+	public String getAppModelVersion() {
+		return getValue(ATTR_APP_MODEL_VERSION).extract();
+	}
+
+	public String getAppModelType() {
+		return getValue(ATTR_APP_MODEL_TYPE).extract();
+	}
+
+	public String getTimezone() {
+		return getValue(ATTR_TIMEZONE).extract();
+	}
+
+	public String[] getMeaningOfAliases() {
+		return getValue(ATTR_MEANING_OF_ALIASES).extract();
 	}
 
 }
